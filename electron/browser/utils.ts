8baf45c217129572de77b945586f527850e1d908
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-03-17 20:52:28 +0000

    feat: use mime-types for inferring content-type

--- electron/browser/utils.ts
+++ electron/browser/utils.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import fsPromises from "fs/promises";
-import path from "path";
+import mimeTypes from "mime-types";
 
 export async function isFileExists(filePath: string) {
   return await fsPromises
@@ -10,33 +10,5 @@ export async function isFileExists(filePath: string) {
 }
 
 export function getContentType(filePath: string) {
-  const fileExtension = path.extname(filePath).toLowerCase();
-  let contentType = "text/plain";
-
-  switch (fileExtension) {
-    case ".html":
-      contentType = "text/html";
-      break;
-    case ".js":
-      contentType = "application/javascript";
-      break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-    case ".jpeg":
-      contentType = "image/jpeg";
-      break;
-    case ".svg":
-      contentType = "image/svg+xml";
-      break;
-  }
-
-  return contentType;
+  return mimeTypes.lookup(filePath) || "text/plain";
 }
