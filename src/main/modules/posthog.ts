74c9b57b31edec74d4c2e8eee819708d36fc5c03
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-05-03 17:41:46 +0100

    feat: enhance PostHog integration with exception capturing and app info

--- src/main/modules/posthog.ts
+++ src/main/modules/posthog.ts
@@ -2,8 +2,9 @@ import { SettingsDataStore } from "@/saving/settings";
 import { app } from "electron";
 import { PostHog } from "posthog-node";
 
-export const client = new PostHog("phc_P8uPRRW5eJj8vMmgMlsgoOmmeNZ9NxBHN6COZQndvfZ", {
-  host: "https://eu.i.posthog.com"
+const client = new PostHog("phc_P8uPRRW5eJj8vMmgMlsgoOmmeNZ9NxBHN6COZQndvfZ", {
+  host: "https://eu.i.posthog.com",
+  enableExceptionAutocapture: true
 });
 
 async function getAnonUserId() {
@@ -16,16 +17,31 @@ async function getAnonUserId() {
   return anonUserId;
 }
 
+function getAppInfoForPosthog() {
+  return {
+    version: app.getVersion(),
+    platform: process.platform,
+    environment: process.env.NODE_ENV
+  };
+}
+
 export async function captureEvent(event: string, properties?: Record<string, unknown>) {
+  const anonUserId = await getAnonUserId();
   client.capture({
-    distinctId: await getAnonUserId(),
+    distinctId: anonUserId,
     event: event,
     properties: {
       ...properties,
-      version: app.getVersion(),
-      platform: process.platform,
-      environment: process.env.NODE_ENV
+      ...getAppInfoForPosthog()
     }
+  });
+}
+
+export async function captureException(error: Error, properties?: Record<string, unknown>) {
+  const anonUserId = await getAnonUserId();
+  client.captureException(error, anonUserId, {
+    ...properties,
+    ...getAppInfoForPosthog()
   });
 }
 
