55b709134ab0ff6ea1d61e88278e1ad8b447f8a0
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-05-13 12:06:17 +0100

    remove

--- src/main/settings/main.ts
+++ src/main/settings/main.ts
@@ -35,10 +35,6 @@ async function createSettingsWindow() {
   registerWindow(WindowType.SETTINGS, "settings", window);
   settingsWindow = window;
 
-  window.webContents.openDevTools({
-    mode: "detach"
-  });
-
   return await new Promise((resolve) => {
     window.once("ready-to-show", () => {
       resolve(window);
