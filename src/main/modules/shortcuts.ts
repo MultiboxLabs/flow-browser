40084463485fbec3a226335f60b5d800d000b49b
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-05-13 09:32:35 +0100

    feat: add shortcuts

--- src/main/modules/shortcuts.ts
+++ src/main/modules/shortcuts.ts
@@ -2,11 +2,72 @@ import { getAllModifiedShortcuts } from "@/saving/shortcuts";
 import { ShortcutAction } from "~/types/shortcuts";
 
 export const shortcuts: ShortcutAction[] = [
+  // Tabs
   {
     id: "tabs.new",
     name: "New Tab",
     shortcut: "CommandOrControl+T",
     category: "Tabs"
+  },
+
+  // Tab
+  {
+    id: "tab.copyUrl",
+    name: "Copy URL",
+    shortcut: "CommandOrControl+Shift+C",
+    category: "Tab"
+  },
+  {
+    id: "tab.reload",
+    name: "Reload",
+    shortcut: "CommandOrControl+R",
+    category: "Tab"
+  },
+  {
+    id: "tab.forceReload",
+    name: "Force Reload",
+    shortcut: "Shift+CommandOrControl+R",
+    category: "Tab"
+  },
+  {
+    id: "tab.close",
+    name: "Close Tab",
+    shortcut: "CommandOrControl+W",
+    category: "Tab"
+  },
+  {
+    id: "tab.toggleDevTools",
+    name: "Toggle DevTools",
+    shortcut: "CommandOrControl+Shift+I",
+    category: "Tab"
+  },
+
+  // Navigation
+  {
+    id: "navigation.goBack",
+    name: "Go Back",
+    shortcut: "CommandOrControl+Left",
+    category: "Navigation"
+  },
+  {
+    id: "navigation.goForward",
+    name: "Go Forward",
+    shortcut: "CommandOrControl+Right",
+    category: "Navigation"
+  },
+
+  // Browser
+  {
+    id: "browser.newWindow",
+    name: "New Window",
+    shortcut: "CommandOrControl+N",
+    category: "Browser"
+  },
+  {
+    id: "browser.toggleSidebar",
+    name: "Toggle Sidebar",
+    shortcut: "CommandOrControl+B",
+    category: "Browser"
   }
 ];
 
