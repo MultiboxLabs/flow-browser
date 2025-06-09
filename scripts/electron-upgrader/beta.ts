cb627fad34ff3ffa89b6dce974f715058b5b3c59
iamEvan <47493765+iamEvanYT@users.noreply.github.com>
2025-06-09 15:23:08 +0100

    fix: beta version

--- scripts/electron-upgrader/beta.ts
+++ scripts/electron-upgrader/beta.ts
@@ -2,27 +2,27 @@ import { findLatestBetaMajorVersion, getCommitHashForTag } from "./github";
 import { updateBunLock, updatePackageJson } from "./updater";
 
 // GRAB RELEASE FROM GITHUB //
-const latestStableVersion = await findLatestBetaMajorVersion();
+const latestBetaVersion = await findLatestBetaMajorVersion();
 
-if (!latestStableVersion) {
-  throw new Error("No stable version found");
+if (!latestBetaVersion) {
+  throw new Error("No beta version found");
 }
 
-const commitHash = await getCommitHashForTag(latestStableVersion);
+const commitHash = await getCommitHashForTag(latestBetaVersion);
 
 if (!commitHash) {
   throw new Error("No commit hash found");
 }
 
-console.log(`Latest stable version: ${latestStableVersion}`);
+console.log(`Latest beta version: ${latestBetaVersion}`);
 console.log(`Commit hash: ${commitHash}`);
 
 // UPDATE PACKAGE.JSON //
-updatePackageJson(latestStableVersion);
+updatePackageJson(latestBetaVersion);
 
 console.log("package.json updated!");
 
 // UPDATE BUN.LOCK //
-updateBunLock(latestStableVersion, commitHash);
+updateBunLock(latestBetaVersion, commitHash);
 
 console.log("bun.lock updated!");
