1d2b4a3c96d59b7597e82d603f6d28f21a2547f3
Maciej Sykuła <maciek.sykula@gmail.com>
2025-06-23 20:40:30 +0200

    refactor: remove useEffect in favor of normal component

--- src/renderer/src/components/analytics/umami.tsx
+++ src/renderer/src/components/analytics/umami.tsx
@@ -1,24 +1,14 @@
-import { useEffect } from "react";
 
 export const UmamiScriptLoader = () => {
+  if (process.env.NODE_ENV !== 'production') return null;
+
   // Umami Analytics
   // Very simple, does not collect or store personal data
   // Does not log what websites you visit, or anything similar
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') return;
-
-    const script = document.createElement('script');
-    script.src = '/umami.js';
-    script.defer = true;
-    script.setAttribute('data-host-url', 'https://umami.iamevan.dev');
-    script.setAttribute('data-website-id', '846df382-cb68-4e59-a97e-76df33a73e90');
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return null;
+  return <script
+    defer
+    src="/umami.js"
+    data-host-url="https://umami.iamevan.dev"
+    data-website-id="846df382-cb68-4e59-a97e-76df33a73e90"
+  />
 };
