9584f939781e81d224a8c8088d601b3795babd7e
iamEvan <evan@iamevan.dev>
2025-09-21 10:25:36 +0100

    feat: improve quit controller

--- src/main/controllers/quit-controller/index.ts
+++ src/main/controllers/quit-controller/index.ts
@@ -4,30 +4,43 @@ import { app } from "electron";
 import { canQuit } from "./handlers/can-quit";
 import { beforeQuit } from "./handlers/before-quit";
 
-let beforeQuitHandlerState: "idle" | "running" | "completed" = "idle";
+type BeforeQuitHandlerState = "idle" | "running" | "completed";
 
-app.on("before-quit", (event) => {
-  if (beforeQuitHandlerState === "completed") {
-    // Let the app quit normally
-    return;
+class QuitController {
+  public beforeQuitHandlerState: BeforeQuitHandlerState;
+
+  constructor() {
+    this.beforeQuitHandlerState = "idle";
+    this._handleBeforeQuit();
   }
 
-  // Prevent the app from quitting if the handler is not completed
-  event.preventDefault();
-
-  // If the handler is idle and the app can quit, run it
-  if (beforeQuitHandlerState === "idle" && canQuit()) {
-    beforeQuitHandlerState = "running";
-
-    const handleBeforeQuit = async () => {
-      const result = await beforeQuit();
-      if (result) {
-        beforeQuitHandlerState = "completed";
-        app.quit();
-      } else {
-        beforeQuitHandlerState = "idle";
+  private _handleBeforeQuit() {
+    app.on("before-quit", (event) => {
+      if (this.beforeQuitHandlerState === "completed") {
+        // Let the app quit normally
+        return;
       }
-    };
-    handleBeforeQuit();
+
+      // Prevent the app from quitting if the handler is not completed
+      event.preventDefault();
+
+      // If the handler is idle and the app can quit, run it
+      if (this.beforeQuitHandlerState === "idle" && canQuit()) {
+        this.beforeQuitHandlerState = "running";
+
+        const handleBeforeQuit = async () => {
+          const result = await beforeQuit();
+          if (result) {
+            this.beforeQuitHandlerState = "completed";
+            app.quit();
+          } else {
+            this.beforeQuitHandlerState = "idle";
+          }
+        };
+        handleBeforeQuit();
+      }
+    });
   }
-});
+}
+
+export const quitController = new QuitController();
