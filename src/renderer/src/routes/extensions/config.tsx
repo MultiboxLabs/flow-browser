84d506678d2e4fba12345797e3e1e9c7a828fb63
Evan <evan@iamevan.dev>
2025-10-30 16:58:06 +0000

    feat: replace `use-query-params` with `nuqs`

--- src/renderer/src/routes/extensions/config.tsx
+++ src/renderer/src/routes/extensions/config.tsx
@@ -1,9 +1,14 @@
 import { ThemeProvider } from "@/components/main/theme";
+import { NuqsProvider } from "@/components/providers/nuqs-provider";
 import { RouteConfigType } from "@/types/routes";
 import { ReactNode } from "react";
 
 export const RouteConfig: RouteConfigType = {
   Providers: ({ children }: { children: ReactNode }) => {
-    return <ThemeProvider forceTheme="dark">{children}</ThemeProvider>;
+    return (
+      <ThemeProvider forceTheme="dark">
+        <NuqsProvider>{children}</NuqsProvider>
+      </ThemeProvider>
+    );
   }
 };
