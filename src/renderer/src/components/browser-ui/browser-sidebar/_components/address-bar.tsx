93b5c4fae9126ca5abc555d1663e2278c7a118b4
Evan <evan@iamevan.dev>
2025-12-19 22:20:53 +0000

    feat: address bar

---
+++ src/renderer/src/components/browser-ui/browser-sidebar/_components/address-bar.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import { SearchIcon } from "lucide-react";
+
+export function AddressBar() {
+  const isPlaceholder = true;
+
+  return (
+    <div className="w-full h-full">
+      <div
+        className={cn(
+          "w-full h-9 rounded-xl",
+          "bg-black/10 hover:bg-black/15",
+          "dark:bg-white/30 dark:hover:bg-white/35",
+          "transition-[background-color] duration-100",
+          "flex items-center p-2 px-3 gap-1.5",
+          isPlaceholder ? "text-white/60" : "text-white"
+        )}
+      >
+        <SearchIcon strokeWidth={2} className="size-4" />
+        <p className={cn("font-[inter] text-sm font-medium truncate")}>
+          {isPlaceholder ? "Search or Enter URL..." : "w3schools.com"}
+        </p>
+      </div>
+    </div>
+  );
+}
