01a2e6df91cf00277e74fc64015e98c75a24dd79
Evan <evan@iamevan.dev>
2025-12-21 01:28:23 +0000

    refactor: adjust animation

--- src/renderer/src/components/browser-ui/update-effect.tsx
+++ src/renderer/src/components/browser-ui/update-effect.tsx
@@ -1,7 +1,7 @@
 import { motion } from "motion/react";
 import { useEffect, useState } from "react";
 
-const ANIMATION_DELAY_SECONDS = 1;
+const ANIMATION_DELAY_SECONDS = 0.2;
 
 export function UpdateEffect() {
   const [completed, setCompleted] = useState(false);
@@ -23,15 +23,15 @@ export function UpdateEffect() {
     <>
       <motion.div
         className="browser-update-animation"
-        initial={{ top: "100%", opacity: 0.5 }}
-        animate={{ top: "-100%", opacity: 1 }}
-        transition={{ duration: 0.6, delay: ANIMATION_DELAY_SECONDS }}
+        initial={{ top: "150%", opacity: 0.5 }}
+        animate={{ top: "-150%", opacity: 1 }}
+        transition={{ duration: 1, delay: ANIMATION_DELAY_SECONDS }}
       ></motion.div>
       <motion.div
         className="browser-update-animation-border"
         initial={{ "--background-top": "150%" }}
         animate={{ "--background-top": "-50%" }}
-        transition={{ duration: 0.3, delay: ANIMATION_DELAY_SECONDS + 0.08 }}
+        transition={{ duration: 0.5, delay: ANIMATION_DELAY_SECONDS + 0.08 }}
       ></motion.div>
     </>
   );
