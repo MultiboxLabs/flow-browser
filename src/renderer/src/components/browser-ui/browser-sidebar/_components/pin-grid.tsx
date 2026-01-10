41fbdd3107d18a6b61428e7b1fe3d80b9e714f1f
Evan <evan@iamevan.dev>
2026-01-10 16:43:37 +0000

    a

--- src/renderer/src/components/browser-ui/browser-sidebar/_components/pin-grid.tsx
+++ src/renderer/src/components/browser-ui/browser-sidebar/_components/pin-grid.tsx
@@ -3,37 +3,7 @@ import "./pin.css";
 import { cn } from "@/lib/utils";
 import { useMeasure } from "react-use";
 import { useEffect, useState } from "react";
-
-export function PinnedTabButton({ faviconUrl, isActive }: { faviconUrl: string; isActive: boolean }) {
-  return (
-    <div
-      className={cn(
-        "w-full h-12 rounded-xl overflow-hidden",
-        "bg-black/10 hover:bg-black/15",
-        "dark:bg-white/15 dark:hover:bg-white/20",
-        "transition-[background-color] duration-100",
-        "flex items-center justify-center",
-        isActive && "border-2 border-primary"
-      )}
-    >
-      <div
-        id="overlay"
-        className={cn("size-full", "flex items-center justify-center", isActive && "bg-white/80 dark:bg-white/30")}
-      >
-        <div className="relative size-6">
-          <img
-            src={faviconUrl || undefined}
-            className="absolute rounded-sm user-drag-none object-contain overflow-hidden"
-          />
-          <div className="img-container">
-            <img src={faviconUrl || undefined} className="user-drag-none" />
-          </div>
-        </div>
-        {/* <img src={faviconUrl || undefined} className="img-container" /> */}
-      </div>
-    </div>
-  );
-}
+import { PinnedTabButton } from "@/components/browser-ui/browser-sidebar/_components/pinned-tab-button";
 
 export function PinGrid() {
   const [ref, { width }] = useMeasure<HTMLDivElement>();
