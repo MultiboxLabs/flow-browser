6b2aacd492d4026fa08c05aa6e8750b89e32800d
Evan <evan@iamevan.dev>
2026-03-20 10:44:34 +0000

    feat: add history to bottom extras

--- src/renderer/src/components/browser-ui/browser-sidebar/_components/bottom/bottom-extras-menu.tsx
+++ src/renderer/src/components/browser-ui/browser-sidebar/_components/bottom/bottom-extras-menu.tsx
@@ -1,15 +1,36 @@
+import { PortalPopover } from "@/components/portal/popover";
+import { useSpaces } from "@/components/providers/spaces-provider";
 import { Button } from "@/components/ui/button";
-import { EllipsisVerticalIcon } from "lucide-react";
+import { PopoverTrigger } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { EllipsisVerticalIcon, HistoryIcon } from "lucide-react";
+import { useState } from "react";
 
 export function BottomExtrasMenu() {
+  const [open, setOpen] = useState(false);
+
+  const { isCurrentSpaceLight } = useSpaces();
+  const spaceInjectedClasses = cn(isCurrentSpaceLight ? "" : "dark");
+
   return (
-    <Button
-      size="icon"
-      className="size-8 bg-transparent hover:bg-black/10 dark:hover:bg-white/10"
-      onClick={() => flow.windows.openSettingsWindow()}
-      disabled
-    >
-      <EllipsisVerticalIcon strokeWidth={2} className="w-4 h-4 text-black/80 dark:text-white/80" />
-    </Button>
+    <PortalPopover.Root open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button size="icon" className="size-8 bg-transparent hover:bg-black/10 dark:hover:bg-white/10">
+          <EllipsisVerticalIcon strokeWidth={2} className="w-4 h-4 text-black/80 dark:text-white/80" />
+        </Button>
+      </PopoverTrigger>
+      <PortalPopover.Content className={cn("w-56 p-2 select-none", spaceInjectedClasses)}>
+        <div
+          onClick={() => {
+            flow.tabs.newTab("flow://history", true);
+            setOpen(false);
+          }}
+          className="flex items-center gap-2 px-2 py-1.5 text-sm rounded-sm hover:bg-accent cursor-pointer"
+        >
+          <HistoryIcon className="w-4 h-4" />
+          <span>History</span>
+        </div>
+      </PortalPopover.Content>
+    </PortalPopover.Root>
   );
 }
