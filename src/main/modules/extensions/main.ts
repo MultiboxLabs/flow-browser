1bcbc8be40c5f962ee0cb9a6dcce4b65039c5a72
Evan <evan@iamevan.dev>
2026-03-30 15:45:21 +0100

    chore: remove dead commented code

--- src/main/modules/extensions/main.ts
+++ src/main/modules/extensions/main.ts
@@ -24,38 +24,4 @@ setSessionPartitionResolver(partitionSessionGrabber);
 // Register CRX protocol in default session
 app.whenReady().then(() => {
   ElectronChromeExtensions.handleCRXProtocol(session.defaultSession);
-  /**
-  protocol.handle("crx", async (request) => {
-    const url = URL.parse(request.url);
-
-    if (!url) {
-      return new Response("Invalid URL", { status: 404 });
-    }
-
-    const partition = url?.searchParams.get("partition");
-
-    if (!partition) {
-      return new Response("No partition", { status: 400 });
-    }
-
-    let session: Session | null = null;
-    try {
-      session = partitionSessionGrabber(partition);
-    } catch {
-      // Not found, return 404 below
-    }
-
-    if (!session) {
-      return new Response("Session not found", { status: 404 });
-    }
-
-    const extensions = ElectronChromeExtensions.fromSession(session);
-
-    if (!extensions) {
-      return new Response("Extensions not found", { status: 404 });
-    }
-
-    return extensions.handleCrxRequest(request);
-  });
-  */
 });
