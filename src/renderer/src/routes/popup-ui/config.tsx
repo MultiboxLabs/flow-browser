c5079f8ec61a1d8b9f3cad3a5a07b3dfba637779
Evan <evan@iamevan.dev>
2026-04-17 01:00:59 +0100

    fix: web prompts applying dark/light mode on wrong document (main UI instead of portal)

--- src/renderer/src/routes/popup-ui/config.tsx
+++ src/renderer/src/routes/popup-ui/config.tsx
@@ -1,9 +1,14 @@
+import { ThemeProvider } from "@/components/main/theme";
 import { PortalsProvider } from "@/components/portal/provider";
 import { RouteConfigType } from "@/types/routes";
 import { ReactNode } from "react";
 
 export const RouteConfig: RouteConfigType = {
   Providers: ({ children }: { children: ReactNode }) => {
-    return <PortalsProvider>{children}</PortalsProvider>;
+    return (
+      <ThemeProvider shouldApplyClass={false}>
+        <PortalsProvider>{children}</PortalsProvider>
+      </ThemeProvider>
+    );
   }
 };
